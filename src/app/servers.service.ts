249ef911bf98521b68892d0acd8bf03f7f514c25
christianarredondo <christianarredondo@utexas.edu>
2017-08-15 20:44:47 -0500

    map http response

--- src/app/servers.service.ts
+++ src/app/servers.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http/';
+import { Headers, Http, Response } from '@angular/http/';
+import 'rxjs/Rx';
 
 @Injectable()
 export class ServerService {
   constructor (private http: Http) {}
   storeServers (servers: any[]) {
     const headers = new Headers({'Content-Type': 'application/json'});
-    return this.http.post('https://udemy-ng-http-916fd.firebaseio.com/data.json',
+    // return this.http.post('https://udemy-ng-http-916fd.firebaseio.com/data.json',
+    //   servers,
+    //   {headers: headers});
+    return this.http.put('https://udemy-ng-http-916fd.firebaseio.com/data.json',
       servers,
       {headers: headers});
   }
     getServers () {
-      return this.http.get('https://udemy-ng-http-916fd.firebaseio.com/data.json');
+      return this.http.get('https://udemy-ng-http-916fd.firebaseio.com/data.json')
+        .map(
+          (response: Response) => {
+            const data = response.json();
+            return data;
+          }
+        );
     }
 }
